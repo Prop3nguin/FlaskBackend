f4c9e12937601cef37a44668b5e346ae14418aca
Prop3nguin <Spencer.haley@outlook.com>
2026-05-06 22:17:08 -0500

    BareBones#18

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/")
 def home():
     return "WORKING"
+
+@app.route("/translate", methods=["POST"])
+def translate():
+    return {"result": "test"}
+
+@app.route("/lexicon")
+def lexicon():
+    return {}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
